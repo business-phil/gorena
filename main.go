b661e8c8fa397b0704934d17c61e4770a393f3cf
Phil <phil.boothe@gmail.com>
2018-11-19 20:54:19 -0500

    make gorena a proper executable

--- main.go
+++ main.go
@@ -1,11 +1,50 @@
-package gorena
+package main
 
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/manifoldco/promptui"
 )
+
+type Combatant struct {
+	name      string
+	maxHp     int
+	currentHp int
+	maxDamage int
+}
+
+func (combatant *Combatant) Attack(opponent *Combatant) (int, bool) {
+	rand.Seed(time.Now().Unix())
+
+	damage := rand.Intn(combatant.maxDamage) + 1
+
+	opponent.currentHp -= damage
+
+	fmt.Printf("%s hit %s for %d damage\n", combatant.name, opponent.name, damage)
+
+	return damage, opponent.currentHp <= 0
+}
+
+func (combatant *Combatant) Heal() (int, bool) {
+	rand.Seed(time.Now().Unix())
+
+	atFullHealth := false
+	healthRestored := rand.Intn(combatant.maxDamage) + rand.Intn(combatant.maxDamage) + 2
+
+	if healthRestored+combatant.currentHp > combatant.maxHp {
+		healthRestored = combatant.maxHp - combatant.currentHp
+		atFullHealth = true
+	}
+
+	combatant.currentHp += healthRestored
+
+	fmt.Printf("%s healed for %d hp\n", combatant.name, healthRestored)
+
+	return healthRestored, atFullHealth
+}
 
 func main() {
 	namePrompt := promptui.Prompt{
